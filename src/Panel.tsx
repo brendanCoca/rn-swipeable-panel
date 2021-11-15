dddd0736ff2f4fa67490a9fe0a00c8416bda2c01
Brendan Coca <brendan.coca@gmail.com>
2021-11-14 17:16:39 -0700

    change

--- src/Panel.tsx
+++ src/Panel.tsx
@@ -18,9 +18,9 @@ let FULL_WIDTH = Dimensions.get('window').width;
 let PANEL_HEIGHT = FULL_HEIGHT - 100;
 
 export enum STATUS {
-  CLOSED = 1,
-  SMALL = 2,
-  LARGE = 3,
+  CLOSED = 0,
+  SMALL = 1,
+  LARGE = 2,
 }
 
 interface SwipeablePanelProps {
